e109439822a61ef7be1f9a77ae3b7587b7456b70
LuiiVasquez <luivasque95@gmail.com>
2023-03-06 15:18:44 -0600

    using fetch

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,13 @@ import Form from "./components/Form";
 import List from "./components/List";
 
 // ? contrato que tiene que tener un objeto
-import { Sub } from "./types";
+import { Sub, SubsResponseFromAPI } from "./types";
 
 // ? contrato para los estados de la aplicacion
 interface AppState {
   subs: Array<Sub>;
   newSubsNumner: number;
 }
-
-const INITIAL_STATE = [
-  {
-    nick: "dapelu",
-    subMonths: 3,
-    avatar: "https://i.pravatar.cc/150?u=dapelu",
-    description: "Dapelu hace de moderados a veces",
-  },
-  {
-    nick: "sergio_serrano",
-    subMonths: 2,
-    avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
-  },
-];
 
 function App() {
   const [subs, setSubs] = useState<AppState["subs"]>([]);
@@ -34,17 +20,48 @@ function App() {
   const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setSubs(INITIAL_STATE);
+    const fetchSubs = (): Promise<SubsResponseFromAPI> => {
+      return fetch("http://localhost:3000/subs").then((subs) => subs.json());
+    };
+
+    // ? transformar los datos de la api a datos que entiende la aplicacion
+    // * cambiar de contrato
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromAPI): Array<Sub> => {
+      return apiResponse.map((subFromApi) => {
+        const {
+          nick,
+          months: subMonths,
+          profileURL: avatar,
+          description,
+        } = subFromApi;
+
+        return {
+          nick,
+          description,
+          avatar,
+          subMonths,
+        };
+      });
+    };
+
+    fetchSubs()
+      .then(mapFromApiToSubs)
+      .then(setSubs)
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   const handleNewSub = (newSub: Sub): void => {
     setSubs((subs) => [...subs, newSub]);
+    setNewSubsNumber((n) => n + 1);
   };
 
   return (
     <div className="App" ref={divRef}>
       <h1>Midu subs</h1>
       <List subs={subs} />
+      New Subs: {newSubsNumber}
       <Form onNewSub={handleNewSub} />
     </div>
   );
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,15 @@
+// ? contrato de subs -> formulario y div
 export interface Sub {
   nick: string;
   avatar: string;
   subMonths: number;
   description?: string;
 }
+
+// ? contrato de la API
+export type SubsResponseFromAPI = Array<{
+  nick: string;
+  months: number;
+  profileURL: string;
+  description: string;
+}>;
